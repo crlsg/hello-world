1268165f9f6f953a4d0694cc35ebc7799ae18522
carlos <mjcgallo@tip.edu.ph>
2023-10-02 11:21:38 +0800

    new version

--- Gallo.py
+++ Gallo.py
@@ -1,2 +1,7 @@
+print('Hello may i ask your full name?')
+firstname = input("Enter First Name: ")
+middlename = input("Enter Middle Name: ")
+lastname = input("Enter Last Name: ")
 
+print("Full Name: ",firstname,middlename,lastname)
 
